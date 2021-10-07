5153b73e4c2c2d1a683cbaab0f05d84de53752d9
Brian710 <mmm850710@gmail.com>
2021-10-07 23:17:52 +0800

    Update 1007

--- TMBA/InfoSpot/js/AR.js
+++ TMBA/InfoSpot/js/AR.js
@@ -1,1 +1,89 @@
-var _url="";function Init(){var e=parent.document.getElementById("Content").getAttribute("data-ar"),n=new XMLHttpRequest;n.open("get","../PageInformation.json"),n.send(null),n.onload=function(){var t;200==n.status&&(Photo(t=JSON.parse(n.responseText),e),Title(t,e),_url=t.hotspot_94[e].text,InsertComponent(t,e))}}function Photo(t,e){let n=document.getElementById("ContentImage"),o=document.getElementById("LogoImage");n.setAttribute("src","./images/"+t.hotspot_94[e].image),o.setAttribute("src","./images/"+t.hotspot_94[e].logo)}function Title(t,e){let n=document.getElementById("ContentTitle");n.innerHTML=t.hotspot_94[e].title}function CheckDevice(){var t=navigator.userAgent,e=navigator.userAgent.toLowerCase(),n=-1<t.indexOf("Line"),o=-1<t.indexOf("Instagram"),i=-1<t.indexOf("FBAV"),s="micromessenger"==e.match(/MicroMessenger/i),d="qq"==e.match(/QQ/i),t="weibo"==e.match(/WeiBo/i);document.getElementById("ios_btn").style.display="none",document.getElementById("android_btn").style.display="none",document.getElementById("desk_btn").style.display="none",document.getElementById("inAPP_btn").style.display="none",n||o||i||s||d||t?document.getElementById("inAPP_btn").style.display="":/(iPhone|iPad|iPod|iOS&&!safari)/i.test(e)?document.getElementById("ios_btn").style.display="":/(Android)/i.test(e)?document.getElementById("android_btn").style.display="":document.getElementById("desk_btn").style.display=""}function InsertComponent(t,e){document.getElementById("ARBtn").innerHTML='<div id="android_btn" style="text-align: center;">        <a id="AndroidAR" href='+t.hotspot_94[e].android_ar+'>            <button type="button" class="col-6 btn btn-primary"">點擊AR</button>        </a>    </div>    <div id="ios_btn" style="text-align: center;">        <a rel="ar" id="ar-link" href='+t.hotspot_94[e].ios_ar+' ><img class="" src="" alt="" style="display:none">            <button type="button" class="col-6 btn btn-primary"">點擊AR</button>        </a>    </div>    <div class="row justify-content-center" id="desk_btn">    <p class="ARText col-12">▼掃描點擊AR體驗▼</p>        <img class="col-6" src="./images/'+t.hotspot_94[e].qrcode+'">    </div>    <div class="row justify-content-center" id="inAPP_btn">        <div>            <p>請使用預設瀏覽器<img src="./images/safari-icon.png">&nbsp;<img src="./images/Chrome-icon.png">開啟體驗</p>        </div>    </div>',CheckDevice()}window.onload=function(){Init()},document.getElementById("WebBtn").addEventListener("click",function(){window.open(_url)});
+var _url = "";
+
+window.onload = function(){
+    Init();
+}
+function Init(){
+    let JSON_Key = parent.document.getElementById("Content").getAttribute("data-ar");
+    var url = "../PageInformation.json"
+    var request = new XMLHttpRequest();
+    request.open("get", url);
+    request.send(null);
+    request.onload = function () {
+        if (request.status == 200) {
+            var json = JSON.parse(request.responseText);
+            Photo(json, JSON_Key);
+            Title(json, JSON_Key);
+            _url = json["hotspot_94"][JSON_Key]["text"];
+            InsertComponent(json, JSON_Key);
+        }
+    }
+}
+
+function Photo(json, JSON_Key){
+    let Photo = document.getElementById("ContentImage");
+    let Logo = document.getElementById("LogoImage");
+    Photo.setAttribute('src','./images/'+ json["hotspot_94"][JSON_Key]["image"]);
+    // Logo.setAttribute('src','./images/'+ json["hotspot_94"][JSON_Key]["logo"]);
+}
+
+function Title(json, JSON_Key){
+    let Title = document.getElementById("ContentTitle");
+    Title.innerHTML= json["hotspot_94"][JSON_Key]["title"];
+}
+
+document.getElementById("WebBtn").addEventListener("click", function(){
+    window.open(_url);
+});
+
+function CheckDevice(){
+    var u = navigator.userAgent,
+    ua = navigator.userAgent.toLowerCase(),
+    isLineApp = u.indexOf("Line") > -1,
+    isInstagramApp = u.indexOf("Instagram") > -1,
+    isFbApp = u.indexOf("FBAV") > -1,
+    isWeixinApp = ua.match(/MicroMessenger/i) == "micromessenger",
+    isQQApp = ua.match(/QQ/i) == "qq",
+    isweiboApp = ua.match(/WeiBo/i) == "weibo";
+    document.getElementById("ios_btn").style.display = "none";
+    document.getElementById("android_btn").style.display = "none";
+    document.getElementById("desk_btn").style.display = "none";
+    document.getElementById("inAPP_btn").style.display = "none";
+
+    if (isLineApp || isInstagramApp || isFbApp || isWeixinApp || isQQApp || isweiboApp) {
+        document.getElementById("inAPP_btn").style.display = "";
+    }
+    else if (/(iPhone|iPad|iPod|iOS&&!safari)/i.test(ua)) {
+        document.getElementById("ios_btn").style.display = "";
+    }
+    else if (/(Android)/i.test(ua)) {
+        document.getElementById("android_btn").style.display = "";
+    }
+    else{
+        document.getElementById("desk_btn").style.display = "";
+    };
+}
+
+function InsertComponent(json, JSON_Key){
+    document.getElementById('ARBtn').innerHTML = 
+    '<div id="android_btn" style="text-align: center;">\
+        <a id="AndroidAR" href='+ json["hotspot_94"][JSON_Key]["android_ar"] +'>\
+            <button type="button" class="col-6 btn btn-primary"">點擊AR</button>\
+        </a>\
+    </div>\
+    <div id="ios_btn" style="text-align: center;">\
+        <a rel="ar" id="ar-link" href='+ json["hotspot_94"][JSON_Key]["ios_ar"] +' ><img class="" src="" alt="" style="display:none">\
+            <button type="button" class="col-6 btn btn-primary"">點擊AR</button>\
+        </a>\
+    </div>\
+    <div class="row justify-content-center" id="desk_btn">\
+    <p class="ARText col-12">▼掃描點擊AR體驗▼</p>\
+        <img class="col-6" src="./images/'+ json["hotspot_94"][JSON_Key]["qrcode"] +'">\
+    </div>\
+    <div class="row justify-content-center" id="inAPP_btn">\
+        <div>\
+            <p>請使用預設瀏覽器<img src="./images/safari-icon.png">&nbsp;<img src="./images/Chrome-icon.png">開啟體驗</p>\
+        </div>\
+    </div>';
+    CheckDevice();
+}
--- TMBA/js/IndexPage.js
+++ TMBA/js/IndexPage.js
@@ -1,1 +1,208 @@
-var onEnableiframe="",onEnableroll="",pano="",roll=["roll_a_1","roll_a_2","roll_a_3","roll_a_4","roll_a_5","roll_a_6","roll_a_7","roll_a_8","roll_a_9","roll_a_10","roll_a_11","roll_a_12","roll_a_13","roll_a_14","roll_a_15","roll_a_16"];function AppendLogoImage(){let e=document.createElement("IMG");e.setAttribute("class","Logo"),e.setAttribute("id","TMBALogo"),e.addEventListener("click",()=>{window.open("https://www.tmba.org.tw/")}),pano.appendChild(e)}function SetSpotInit(e){e!=document.getElementById("Content").getAttribute("data")?pano.call("do_animation("+e+", 3000)"):pano.call("do_animation("+e+", 100)")}function SetEnableFrame(e){e!=onEnableiframe&&SetDisableFrame(),onEnableiframe=e,document.getElementById("Content").setAttribute("data",onEnableiframe),pano.call("set(hotspot["+e+"].url, '%FIRSTXML%/add_hotspot/picture/clickNow.png')")}function SetEnableFrame_Other(e){e!=onEnableiframe&&SetDisableFrame(),onEnableiframe="",document.getElementById("Content").setAttribute("data",e)}function SetDisableFrame(){document.getElementById("Content").setAttribute("data",""),pano.call("set(hotspot["+onEnableiframe+"].url, '%FIRSTXML%/add_hotspot/picture/Plus_Btn.png')"),onEnableiframe=""}function SetRollingPaperImage(e){e!=onEnableroll&&""!=onEnableroll&&pano.call("open_or_close_door("+onEnableroll+"); open_or_close_door(door_hitarea_a_1);"),e!=onEnableroll?""!=onEnableroll?setTimeout(()=>{ChangeRollPaper(e),pano.call("open_or_close_door(door_hitarea_a_1);"),onEnableroll=e},1e3):(ChangeRollPaper(e),pano.call("open_or_close_door(door_hitarea_a_1);"),onEnableroll=e):(ChangeRollPaper(e),pano.call("open_or_close_door(door_hitarea_a_1);"),onEnableroll="")}function SetEnableFrame_Wall(e){document.getElementById("Content").setAttribute("data",e)}function SetTimerChangeContent(e){""!=document.getElementById("Content").getAttribute("data-ar")&&null!=document.getElementById("Content").getAttribute("data-ar")?"UB660"==document.getElementById("Content").getAttribute("data-ar")?(document.getElementById("Content").setAttribute("data-ar","VTL1600A"),pano.call("set(hotspot["+e+"].url, %FIRSTXML%/add_hotspot/picture/VTL1600A.png)")):(document.getElementById("Content").setAttribute("data-ar","UB660"),pano.call("set(hotspot["+e+"].url, %FIRSTXML%/add_hotspot/picture/UB-660.png)")):document.getElementById("Content").setAttribute("data-ar","UB660"),setTimeout(function(){SetTimerChangeContent(e)},5e3)}function ChangeRollPaper(o){if(o!=onEnableroll)for(let e=0;e<roll.length;e++)"door_hitarea11"==o?e<10?pano.call("set(hotspot["+roll[e]+"].url, 'images/rollingpaper/Q1/Q1_0"+e+".png')"):pano.call("set(hotspot["+roll[e]+"].url, 'images/rollingpaper/Q1/Q1_"+e+".png')"):"door_hitarea12"==o?e<10?pano.call("set(hotspot["+roll[e]+"].url, 'images/rollingpaper/Q2/Q2_0"+e+".png')"):pano.call("set(hotspot["+roll[e]+"].url, 'images/rollingpaper/Q2/Q2_"+e+".png')"):"door_hitarea13"==o?e<10?pano.call("set(hotspot["+roll[e]+"].url, 'images/rollingpaper/Q3/Q3_0"+e+".png')"):pano.call("set(hotspot["+roll[e]+"].url, 'images/rollingpaper/Q3/Q3_"+e+".png')"):"door_hitarea14"==o?e<10?pano.call("set(hotspot["+roll[e]+"].url, 'images/rollingpaper/Q4/Q4_0"+e+".png')"):pano.call("set(hotspot["+roll[e]+"].url, 'images/rollingpaper/Q4/Q4_"+e+".png')"):"door_hitarea15"==o&&(e<10?pano.call("set(hotspot["+roll[e]+"].url, 'images/rollingpaper/Q5/Q5_0"+e+".png')"):pano.call("set(hotspot["+roll[e]+"].url, 'images/rollingpaper/Q5/Q5_"+e+".png')"))}function ChangeSceneCloseFrame(){pano.call("set(visible,false);tween(layer[IFRAME_HTML].oy, -350);tween(layer[IFRAME_HTML].alpha, 0);tween(layer[freim_bg_Adhs].alpha, 0);set(layer[fon_zad_plan_adhs].enabled, false);tween(layer[fon_zad_plan_adhs].alpha, 0);delayedcall(0.3,removelayer(IFRAME_HTML);removelayer(freim_bg_Adhs);"),SetDisableFrame(),onEnableroll=""}function DisplayLogo(e){"true"==e?document.getElementById("TMBALogo").style.display="":"false"==e&&(document.getElementById("TMBALogo").style.display="none")}window.onload=function(){embedpano({swf:"tour.swf",xml:"tour.xml",target:"pano",html5:"auto",mobilescale:1,passQueryParameters:!0}),pano=parent.window.document.getElementById("krpanoSWFObject"),AppendLogoImage(),SetTimerChangeContent("hotspot_94")};
+var onEnableiframe = "";
+var onEnableroll = "";
+var pano = "";
+var roll = ["roll_a_1","roll_a_2","roll_a_3","roll_a_4","roll_a_5",
+            "roll_a_6","roll_a_7","roll_a_8","roll_a_9","roll_a_10",
+            "roll_a_11","roll_a_12","roll_a_13","roll_a_14","roll_a_15","roll_a_16"];
+
+window.onload = function(){
+    embedpano({swf:"tour.swf", xml:"tour.xml", target:"pano", html5:"auto", mobilescale:1.0, passQueryParameters:true});
+    pano = parent.window.document.getElementById("krpanoSWFObject");
+    AppendLogoImage();
+    SetTimerChangeContent("hotspot_94");
+}
+
+function AppendLogoImage(){
+    let _img = document.createElement('IMG');
+    _img.setAttribute("class", "Logo");
+    _img.setAttribute("id", "TMBALogo");
+    _img.addEventListener('click', ()=>{
+        window.open("https://www.tmba.org.tw/");
+    });
+    let _guide = document.createElement('IMG');
+    _guide.setAttribute("class","UserGuide");
+    _guide.setAttribute("id","UserGuide");
+    _guide.addEventListener('click', ()=>{
+        let _guideImage = document.createElement('DIV');
+        _guideImage.setAttribute("class","GuideContainer");
+        _guideImage.setAttribute("id","GuideContainer");
+        let _Image = document.createElement("IMG");
+        _Image.setAttribute("class","GuideImage");
+        if(window.innerWidth <= 1024){
+            _Image.style.content = "url('../images/Guide_phone.png')";
+        }
+        else{
+            _Image.style.content = "url('../images/Guide.jpg')";
+        }
+        
+        let _Close = document.createElement('IMG');
+        _Close.setAttribute("class","GuideClose");
+        _Close.style.content = "url('../images/close.png')";
+        _Close.addEventListener('click', ()=>{
+            document.getElementById("GuideContainer").remove();
+        });
+        let _BG = document.createElement('DIV');
+        _BG.setAttribute("class","GUideBG");
+        _guideImage.appendChild(_Image);
+        _guideImage.appendChild(_Close);
+        _guideImage.appendChild(_BG);
+        pano.appendChild(_guideImage);
+    });
+    pano.appendChild(_guide);
+    pano.appendChild(_img);
+}
+
+function SetSpotInit(name){
+    if(name != document.getElementById("Content").getAttribute("data")){
+        pano.call("do_animation(" + name + ", 3000)");
+    }
+    else{
+        pano.call("do_animation(" + name + ", 100)");
+    }
+}
+
+function SetEnableFrame(name){
+    if(name != onEnableiframe){
+        SetDisableFrame();
+    }
+    onEnableiframe = name;
+    document.getElementById("Content").setAttribute("data", onEnableiframe);
+    pano.call("set(hotspot[" + name + "].url, '%FIRSTXML%/add_hotspot/picture/clickNow.png')");
+}
+
+function SetEnableFrame_Other(name){
+    if(name != onEnableiframe){
+        SetDisableFrame();
+    }
+    onEnableiframe = name;
+    document.getElementById("Content").setAttribute("data", name);
+    pano.call("set(hotspot[" + name + "].url, '%FIRSTXML%/add_hotspot/picture/Btn_m_click.png')");
+}
+
+function SetDisableFrame(){
+    document.getElementById("Content").setAttribute("data", "");
+    if(onEnableiframe.includes("other")){
+        pano.call("set(hotspot[" + onEnableiframe + "].url, '%FIRSTXML%/add_hotspot/picture/Btn_m_o.png')");
+    }
+    else{
+        pano.call("set(hotspot[" + onEnableiframe + "].url, '%FIRSTXML%/add_hotspot/picture/Plus_Btn.png')");
+    }
+    onEnableiframe = "";
+}
+
+function SetRollingPaperImage(name){
+    if(name != onEnableroll && onEnableroll != ""){
+        pano.call("open_or_close_door("+ onEnableroll +"); open_or_close_door(door_hitarea_a_1);");
+    }
+    if(name != onEnableroll){
+        if(onEnableroll != ""){
+            console.log(name);
+            setTimeout(() => {
+                ChangeRollPaper(name);
+                pano.call("open_or_close_door(door_hitarea_a_1);");
+                onEnableroll = name;
+            }, 1000);
+        }
+        else{
+            ChangeRollPaper(name);
+            pano.call("open_or_close_door(door_hitarea_a_1);");
+            onEnableroll = name;
+        }
+    }
+    else{
+        ChangeRollPaper(name);
+        pano.call("open_or_close_door(door_hitarea_a_1);");
+        onEnableroll = "";
+    }
+}
+
+function SetEnableFrame_Wall(name){
+    document.getElementById("Content").setAttribute("data", name);
+}
+
+function SetTimerChangeContent(name){
+    if(document.getElementById("Content").getAttribute("data-ar") != "" && document.getElementById("Content").getAttribute("data-ar") != null){
+        if(document.getElementById("Content").getAttribute("data-ar") == "UB660"){
+            document.getElementById("Content").setAttribute("data-ar", "VTL1600A")
+            pano.call("set(hotspot["+ name +"].url, %FIRSTXML%/add_hotspot/picture/VTL1600A.png)");
+        }
+        else{
+            document.getElementById("Content").setAttribute("data-ar", "UB660")
+            pano.call("set(hotspot["+ name +"].url, %FIRSTXML%/add_hotspot/picture/UB-660.png)");
+        }
+    }
+    else{
+        document.getElementById("Content").setAttribute("data-ar", "UB660");
+    }
+
+    setTimeout(function(){
+        SetTimerChangeContent(name);
+    }, 5000);
+}
+
+function ChangeRollPaper(name){
+    if(name != onEnableroll){
+        for(let i=0; i < roll.length; i++){
+            if(name == "door_hitarea11"){
+                if(i < 10){
+                    pano.call("set(hotspot["+ roll[i] +"].url, 'images/rollingpaper/Q1/Q1_0"+ i +".png')");
+                }
+                else{
+                    pano.call("set(hotspot["+ roll[i] +"].url, 'images/rollingpaper/Q1/Q1_"+ i +".png')");
+                }
+            }
+            else if(name == "door_hitarea12"){
+                if(i < 10){
+                    pano.call("set(hotspot["+ roll[i] +"].url, 'images/rollingpaper/Q2/Q2_0"+ i +".png')");
+                }
+                else{
+                    pano.call("set(hotspot["+ roll[i] +"].url, 'images/rollingpaper/Q2/Q2_"+ i +".png')");
+                }
+            }
+            else if(name == "door_hitarea13"){
+                if(i < 10){
+                    pano.call("set(hotspot["+ roll[i] +"].url, 'images/rollingpaper/Q3/Q3_0"+ i +".png')");
+                }
+                else{
+                    pano.call("set(hotspot["+ roll[i] +"].url, 'images/rollingpaper/Q3/Q3_"+ i +".png')");
+                }
+            }
+            else if(name == "door_hitarea14"){
+                if(i < 10){
+                    pano.call("set(hotspot["+ roll[i] +"].url, 'images/rollingpaper/Q4/Q4_0"+ i +".png')");
+                }
+                else{
+                    pano.call("set(hotspot["+ roll[i] +"].url, 'images/rollingpaper/Q4/Q4_"+ i +".png')");
+                }
+            }
+            else if(name == "door_hitarea15"){
+                if(i < 10){
+                    pano.call("set(hotspot["+ roll[i] +"].url, 'images/rollingpaper/Q5/Q5_0"+ i +".png')");
+                }
+                else{
+                    pano.call("set(hotspot["+ roll[i] +"].url, 'images/rollingpaper/Q5/Q5_"+ i +".png')");
+                }
+            }
+        }
+    }
+}
+
+function ChangeSceneCloseFrame(){
+    pano.call("set(visible,false);tween(layer[IFRAME_HTML].oy, -350);tween(layer[IFRAME_HTML].alpha, 0);tween(layer[freim_bg_Adhs].alpha, 0);set(layer[fon_zad_plan_adhs].enabled, false);tween(layer[fon_zad_plan_adhs].alpha, 0);delayedcall(0.3,removelayer(IFRAME_HTML);removelayer(freim_bg_Adhs);")
+    SetDisableFrame();
+    onEnableroll = "";
+}
+
+function DisplayLogo(status){
+    if(status == "true"){
+        document.getElementById("TMBALogo").style.display = "";
+    }
+    else if(status == "false"){
+        document.getElementById("TMBALogo").style.display = "none";
+    }
+}
+
+function InitRollScene(){
+    pano.call("open_or_close_door(door_hitarea15);");
+    SetRollingPaperImage("door_hitarea15");
+}
