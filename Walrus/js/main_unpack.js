364ef63ee3c8130cfddf8cea0e2a20967b63a1c5
Brian710 <mmm850710@gmail.com>
2021-10-29 17:07:25 +0800

    Update main_unpack.js

--- Walrus/js/main_unpack.js
+++ Walrus/js/main_unpack.js
@@ -520,25 +520,29 @@ function G_WaveCanvas(){
     $("#pano").append($(_CanvasLogoDiv));
 
     WaveCanvas = new GameCanvas();
-    time = 80;
+    time = 30;
     loop();
     $("#pano").attr("data-Change","ON");
+    timer.stop();
     timer.start();
 }
 
 function loop(){
-    background("rgba(255,255,255,0.1)");
-    var d=[function(g){
-        return Math.sin(g * 6 + time * 0.01) * (100) * Math.sin(time * 0.03)
-    },function(g){
-        return(Math.sin(g * 6 + time * 0.01) * (120) + 20) * Math.sin(time * 0.03)
-    }];
-    var b=["rgb(255,217,0)","rgb(0,67,143)"];
-    for(var f=0;f<2;f++){
-        var a=b[f];
-        var c=d[f];
+    background("rgba(255,255,255,1)");
+    let d=[
+        function(g){
+            return Math.sin(g * 6 + time * 0.01) * (100) * Math.sin(time * 0.03)
+        },
+        function(g){
+            return (Math.sin(g * 6 + time * 0.01) * (120) + 20) * Math.sin(time * 0.03)
+        }
+    ];
+    let b=["rgb(255,217,0)","rgb(0,67,143)"];
+    for(let f=0; f<2; f++){
+        let a=b[f];
+        let c=d[f];
         beginPath();
-        for(var e=0;e<=1+0.01;e+=0.01){
+        for(let e=0; e<=1+0.01; e+=0.01){
             lineTo(e * width, 100 + c(e))
         }        
         lineTo(width,height);
@@ -547,7 +551,7 @@ function loop(){
         fillStyle(a);
         fill()
     }
-    time++;
+    time+=3;
     update();
     requestAnimationFrame(loop)
 };
