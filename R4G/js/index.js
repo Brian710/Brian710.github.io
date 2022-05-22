83a868a3ccb91a133ce8c24911894eaf78db4d7e
Brian710 <mmm850710@gmail.com>
2022-05-22 15:51:18 +0800

    add 0522

--- R4G/js/index.js
+++ R4G/js/index.js
@@ -14,3 +14,40 @@ window.onload = function(){
     });
 };
 
+let windowButton = document.getElementById("StartButton");
+windowButton.addEventListener("click", ()=>{
+    alert("開始養樹");
+});
+windowButton.addEventListener("mouseover", ()=>{
+    windowButton.style.backgroundColor = "#009C9A";
+    windowButton.children[0].style.color = "#FFFFFF";
+});
+windowButton.addEventListener("mouseout", ()=>{
+    windowButton.style.backgroundColor = null;
+    windowButton.children[0].style.color = "#009C9A";
+});
+
+let RunRight = document.getElementById("RunRight");
+let RunLeft = document.getElementById("RunLeft");
+let RunCenter = document.getElementById("RunCenter");
+anime({
+    targets: RunRight,
+    translateX: screen.width,
+    duration: 4300,
+    easing: 'linear',
+    loop: true
+});
+anime({
+    targets: RunLeft,
+    translateX: screen.width,
+    duration: 4700,
+    easing: 'linear',
+    loop: true
+});
+anime({
+    targets: RunCenter,
+    translateX: screen.width,
+    duration: 4500,
+    easing: 'linear',
+    loop: true
+});
---
+++ R4G/js/loadanimation.js
@@ -0,0 +1,63 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('BGItem'),
+    path: './images/BGItem.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+});
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('Cloud'),
+    path: './images/Cloud.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+});
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('Dog'),
+    path: './images/Dog.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+});
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('RunRight'),
+    path: './images/RunRight.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+});
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('RunLeft'),
+    path: './images/RunLeft.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+});
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('RunCenter'),
+    path: './images/RunCenter.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+});
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('LeftTree'),
+    path: './images/LeftTree.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+});
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('RightTree'),
+    path: './images/RightTree.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+});
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('FrontPeople'),
+    path: './images/FrontPeople.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+});
