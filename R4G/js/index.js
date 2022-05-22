76949fc2aa113c625aa5481a8a2c9d820b35f1af
Brian710 <mmm850710@gmail.com>
2022-05-22 17:29:09 +0800

    add safari 100vw 0522

--- R4G/js/index.js
+++ R4G/js/index.js
@@ -1,4 +1,6 @@
+let vw = window.innerWidth * 0.01;
 let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vw', `${vw}px`);
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 window.onload = function(){
