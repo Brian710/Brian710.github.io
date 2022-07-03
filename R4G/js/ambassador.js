84aac46bc5a55520bdef0b556e19c33755c8f3ed
Brian710 <mmm850710@gmail.com>
2022-07-03 18:44:45 +0800

    Update ambassador.js

--- R4G/js/ambassador.js
+++ R4G/js/ambassador.js
@@ -3,11 +3,14 @@ let checkmycertificate_2 = document.getElementById("certificate");
 
 window.addEventListener('load', ()=>{
     document.getElementById("mycertificate").style.display = "none";
+    document.getElementById("nav-bar-conatiner").style.zIndex = "10000";
 });
 
 checkmycertificate_1.addEventListener('click', ()=>{
     document.getElementById("mycertificate").style.display = "";
+    document.getElementById("nav-bar-conatiner").style.zIndex = "9999";
 });
 checkmycertificate_2.addEventListener('click', ()=>{
     document.getElementById("mycertificate").style.display = "";
+    document.getElementById("nav-bar-conatiner").style.zIndex = "9999";
 });
