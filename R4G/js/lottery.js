5a7952dc09ddbd29c0588ad814d448c3ca976e30
Brian710 <mmm850710@gmail.com>
2022-08-14 17:21:35 +0800

    update lottery
    更新抽好禮頁面及其排版

--- R4G/js/lottery.js
+++ R4G/js/lottery.js
@@ -2,7 +2,13 @@ let winner_list_container = document.querySelector("div.winners-list");
 let list_time_block;
 
 window.addEventListener("load", ()=>{
-    
+    if(window.innerWidth >= 1024){
+        let br = document.getElementsByTagName("br");
+        let brlist = Array.prototype.slice.call(br);
+        for(let i = 0; i < brlist.length; i++){
+            brlist[i].style.display = "none";
+        }
+    }
 });
 
 document.getElementById("btnEarly").addEventListener('click', ()=>{
